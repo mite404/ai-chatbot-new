5287653705e2134488c50b1c8c5a049db352cd46
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-01 11:31:37 -0400

    Successfully added `better-auth` and pushed required tables to SupabaseDB

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { authClient } from "lib/auth-client";
+import { SignIn } from "./signin";
+import { SignUp } from "./signup";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +12,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  const { data } = authClient.useSession()
+
+  if (data) {
+    return <div>
+      <h1>AI Chat Bot</h1>
+      <SignIn />
+      <SignUp />
+    </div>
+
+  }
 }
---
+++ db/index.ts
@@ -0,0 +1,46 @@
+// import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+const client = postgres(process.env.DATABASE_URL!, { prepare: false })
+export const db = drizzle({ client });
+
+// FOR TESTING DB CONNECTION
+//
+// import { eq } from 'drizzle-orm';
+// import { users } from './schema'
+
+// FOR TESTING DB CONNECTION
+//
+async function main() {
+  const users: typeof users.$inferInsert = {
+    name: 'John',
+    email: 'john123@test.com'
+  };
+
+  await db.insert(users).values(user);
+  console.log('New user created!')
+
+  const allUsers = await db.select().from(users);
+  console.log('Getting all users from the database:', allUsers)
+  /*
+  const users: {
+    id: number;
+    name: string;
+    email: string;
+  }[]
+  */
+
+  await db
+    .update(users)
+    .set({ name: 'Steve', })
+    .where(eq(users.email, user.email))
+  console.log('User info updated!')
+
+  await db
+    .delete(users)
+    .where(eq(users.email, user.email))
+  console.log('User deleted!')
+}
+
+// main();
