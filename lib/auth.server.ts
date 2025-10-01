ec39fe2a739af69c48bac16b0983ae6bd3892081
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-01 14:51:55 -0400

    Successfully rendering `SignIn` & `SignUp` components. Cleaned up `app/lib` files

--- lib/auth.server.ts
+++
@@ -1,8 +0,0 @@
-import { betterAuth } from "better-auth"
-
-export const auth = betterAuth({
-  database: {
-    provider: "postgres", //change this to your database provider
-    url: process.env.DATABASE_URL, // path to your database or connection string
-  }
-})
