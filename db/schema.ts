49f8afbbd643b8048f90d4563c58716372f2f27b
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-01 11:34:58 -0400

    Successfully added `better-auth` and pushed required tables to SupabaseDB (#2)

--- db/schema.ts
+++ db/schema.ts
@@ -1,21 +1,20 @@
 import { pgTable, varchar, uuid, boolean, timestamp, text } from "drizzle-orm/pg-core";
 
 // Export ALL Better Auth tables
-// export * from '../../auth-schema'
-// import { user } from '../../auth-schema'
+export * from './auth-schema'
+import { user } from './auth-schema'
 
 
 // betterAuth CREATES IT'S OWN 'user' TABLE ??
-export const users = pgTable("users", {
-  id: uuid().primaryKey().defaultRandom(),
-  name: varchar({ length: 255 }).notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+// export const users = pgTable("users", {
+//   id: uuid().primaryKey().defaultRandom(),
+//   name: varchar({ length: 255 }).notNull(),
+//   email: varchar({ length: 255 }).notNull().unique(),
+// });
 
 export const conversations = pgTable("conversations", {
   id: uuid().primaryKey().defaultRandom(),
-  // userId: text().notNull().references(() => user.id),
-  userId: text().notNull(),
+  userId: text().notNull().references(() => user.id),
   created_at: timestamp().defaultNow().notNull(),
   title: varchar({ length: 255 }).notNull()
 })
