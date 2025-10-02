39fb77dfdfe0c2722c54328da50f12885cfcca58
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-01 23:21:08 -0400

    Installed `Vercel AI SDK` updated `.env` AI API

--- app/db/index.ts
+++ app/db/index.ts
@@ -15,35 +15,35 @@ export const db = drizzle({ client });
 
 // FOR TESTING DB CONNECTION
 //
-async function main() {
-  const users: typeof users.$inferInsert = {
-    name: 'John',
-    email: 'john123@test.com'
-  };
+// async function main() {
+//   const users: typeof users.$inferInsert = {
+//     name: 'John',
+//     email: 'john123@test.com'
+//   };
 
-  await db.insert(users).values(user);
-  console.log('New user created!')
+//   await db.insert(users).values(user);
+//   console.log('New user created!')
 
-  const allUsers = await db.select().from(users);
-  console.log('Getting all users from the database:', allUsers)
-  /*
-  const users: {
-    id: number;
-    name: string;
-    email: string;
-  }[]
-  */
+//   const allUsers = await db.select().from(users);
+//   console.log('Getting all users from the database:', allUsers)
+//   /*
+//   const users: {
+//     id: number;
+//     name: string;
+//     email: string;
+//   }[]
+//   */
 
-  await db
-    .update(users)
-    .set({ name: 'Steve', })
-    .where(eq(users.email, user.email))
-  console.log('User info updated!')
+//   await db
+//     .update(users)
+//     .set({ name: 'Steve', })
+//     .where(eq(users.email, user.email))
+//   console.log('User info updated!')
 
-  await db
-    .delete(users)
-    .where(eq(users.email, user.email))
-  console.log('User deleted!')
-}
+//   await db
+//     .delete(users)
+//     .where(eq(users.email, user.email))
+//   console.log('User deleted!')
+// }
 
 // main();
