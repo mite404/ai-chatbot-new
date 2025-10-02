22553858d96d70565a2569cf7cb31a98a36ac118
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-02 17:51:52 -0400

    Implementing UI styling on chat messages

--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -25,17 +25,18 @@ export default function Chat() {
 
   return (
 
-    <>
+    // <div className="flex flex-col h-screen">
+    <div className="flex-1 overflow-y-auto p-4 bg-gray-200">
       {/* background message bubbles will be laid on top of */}
-      <div className="bg-gray-200 p-4 flex-col outline-1 ">
+      <div className="bg-gray-200 p-4 flex flex-col-reverse gap-3">
 
         {messages.map((message) => (
 
           <div key={message.id} className={`flex whitespace-pre-wrap ${message.role === 'user' ? 'justify-end' : 'justify-start'}`}>
 
             {/* a single message bubble (the colored background box) */}
-            <div className={`px-4 py-2 bg-primary text-primary-foreground ${message.role === 'user' ? 'bg-yellow-200' : 'bg-gray-200'}`}>
-              <strong>{message.role === 'user' ? 'User: ' : 'AI: '}</strong>
+            <div className={`px-4 py-2 text-primary-foreground ${message.role === 'user' ? 'bg-yellow-200' : 'bg-gray-300'}`}>
+              {/* <strong>{message.role === 'user' ? 'User: ' : 'AI: '}</strong> */}
 
               {message.parts.map((part, i) => {
                 if (part.type === 'text') {
@@ -48,22 +49,24 @@ export default function Chat() {
         ))}
       </div>
 
-      <div>
-        <form onSubmit={(e) => {
-          e.preventDefault()
-          sendMessage({ text: input })
-          setInput('')
-        }}>
+      <div className="p-4 border-t">
+        <form className="flex gap-2"
+          onSubmit={(e) => {
+            e.preventDefault()
+            sendMessage({ text: input })
+            setInput('')
+          }}>
 
 
-          <Input type="text"
+          <Input className="font-head flex-1"
+            type="text"
             value={input}
-            className="font-head"
             onChange={(e) => setInput(e.target.value)}
             placeholder="Enter message..." />
           <Button type="submit">Send</Button>
         </form>
       </div>
-    </>
+    </div>
+    // </div >
   )
 }
